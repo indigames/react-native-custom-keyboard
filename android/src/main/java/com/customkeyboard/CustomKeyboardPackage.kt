b4f8b450ad5bb4f1a59fae1dc8761b56feb4dd6a
trungnhm1998 <trung.nguyenhoangminh1998@gmail.com>
2023-03-20 11:04:12 +0700

    chore: initial commit

---
+++ android/src/main/java/com/customkeyboard/CustomKeyboardPackage.kt
@@ -0,0 +1,17 @@
+package com.customkeyboard
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class CustomKeyboardPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(CustomKeyboardModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CustomKeyboardExample
+//
+
+import Foundation
